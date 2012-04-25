53ae3655d3ba7a0e300da385b034cdab9101692d
Tim Rakowski <tim.rakowski@googlemail.com>
2012-04-26 00:28:12 +0200

    Augmented the grammar to include program, super combinator, let and letrec non-terminals.

--- parser.h
+++ parser.h
@@ -16,6 +16,60 @@ ostream& operator<<(ostream& s, AST_Node& n) {
   return s;
 }
 
+struct PNode : public AST_Node {
+  AST_Node* sc;
+  AST_Node* scs;
+  PNode(AST_Node* _sc, AST_Node* _scs) : sc(_sc), scs(_scs) {}
+  ~PNode() {
+    if (sc)
+      delete sc;
+    if (scs)
+      delete scs;
+  }
+
+  void writeTo(ostream& s) {
+    sc->writeTo(s);
+    s << ";\n";
+    scs->writeTo(s);
+  }
+};
+
+struct SNode : public AST_Node {
+  string id;
+  vector<string> ids;
+  AST_Node* body;
+  SNode(string&& _id, vector<string>&& _ids, AST_Node* _body) : id(forward<string>(_id)), ids(forward<vector<string>>(_ids)), body(_body) {}
+  ~SNode() {
+    if (body)
+      delete body;
+  }
+
+  void writeTo(ostream& s) {
+    s << id << " " << show(ids) << " = ";
+    body->writeTo(s);
+  }
+};
+
+struct LetNode : public AST_Node {
+  AST_Node* p;
+  AST_Node* l;
+  bool rec;
+  LetNode(AST_Node* _p, AST_Node* _l, bool _rec) : p(_p), l(_l), rec(_rec) {}
+  ~LetNode() {
+    if (p)
+      delete p;
+    if (l)
+      delete l;
+  }
+
+  void writeTo(ostream& s) {
+    s << (rec ? "letrec " : "let ");
+    p->writeTo(s);
+    s << " in ";
+    l->writeTo(s);
+  }
+};
+
 struct AbsNode : public AST_Node {
   vector<string> id;
   AST_Node* body;
@@ -110,6 +164,7 @@ struct Parser {
           parseDR(ids);
           return;
         }
+      case '=':
       case '.':
         return;
       default:
@@ -173,7 +228,7 @@ struct Parser {
         return parseBR(new AppNode(a, parseC()));
         break;
       case ')':
-      case EOF:
+      case ';':
         return a;
       default:
         cerr << "Error: " << t->tag << endl;
@@ -194,15 +249,19 @@ struct Parser {
     }
   }
 
-  AST_Node* parseA() {
-    cout << "parseA()" << endl;
+  AST_Node* parseL() {
+    cout << "parseL()" << endl;
+    bool rec = false;
     switch (t->tag) {
-      case '\\':
+      case (unsigned)Token::type::LETREC:
+        rec = true;
+      case (unsigned)Token::type::LET:
         {
-          ignoreMatch('\\');
-          auto ids = parseD();
-          ignoreMatch('.');
-          return new AbsNode(std::move(ids), parseA());
+          ignoreMatch(t->tag);
+          auto a = parseP();
+          ignoreMatch((unsigned)Token::type::IN);
+          auto l = parseL();
+          return new LetNode(a, l, rec);
         }
       case '(':
       case (unsigned)Token::type::VAR:
@@ -214,8 +273,83 @@ struct Parser {
     }
   }
 
+  AST_Node* parseA() {
+    cout << "parseA()" << endl;
+    switch (t->tag) {
+      case '\\':
+        {
+          ignoreMatch('\\');
+          auto ids = parseD();
+          ignoreMatch('.');
+          return new AbsNode(std::move(ids), parseA());
+        }
+      case '(':
+      case (unsigned)Token::type::LET:
+      case (unsigned)Token::type::LETREC:
+      case (unsigned)Token::type::VAR:
+      case (unsigned)Token::type::NUM:
+        return parseL();
+      default:
+        cerr << "Error: " << t->tag << endl;
+        throw exception();
+    }
+  }
+
+  AST_Node* parseS() {
+    cout << "parseS()" << endl;
+    switch (t->tag) {
+      case (unsigned)Token::type::VAR:
+        {
+          Var* id = dynamic_cast<Var*>(match((unsigned)Token::type::VAR)); //TODO: static_cast, if not debugging
+          auto _id = std::move(id->val);
+          delete id;
+          vector<string> ids;
+          parseDR(ids);
+          ignoreMatch('=');
+          auto a = parseA();
+          return new SNode(std::move(_id), std::move(ids), a);
+        }
+      default:
+        cerr << "Error: " << t->tag << endl;
+        throw exception();
+    }
+  }
+
+  AST_Node* parsePR(AST_Node* a) {
+    cout << "parsePR()" << endl;
+    switch (t->tag) {
+      case (unsigned)Token::type::VAR:
+        {
+          AST_Node* s = parseS();
+          ignoreMatch(';');
+          return parsePR(new PNode(a, s));
+        }
+      case (unsigned)Token::type::IN:
+      case EOF:
+        return a;
+      default:
+        cerr << "Error: " << t->tag << endl;
+        throw exception();
+    }
+  }
+
+  AST_Node* parseP() {
+    cout << "parseP()" << endl;
+    switch (t->tag) {
+      case (unsigned)Token::type::VAR:
+        {
+          AST_Node* s = parseS();
+          ignoreMatch(';');
+          return parsePR(s);
+        }
+      default:
+        cerr << "Error: " << t->tag << endl;
+        throw exception();
+    }
+  }
+
   AST_Node* parse() {
-    return parseA();
+    return parseP();
   }
 };
 
