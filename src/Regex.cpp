cfb91afbf9fbd8897667025e7d392e4baa38ea89
Tim Rakowski <tim.rakowski@googlemail.com>
2019-08-25 13:22:44 +0200

    Clean up Lexer::getToken

--- src/Regex.cpp
+++ src/Regex.cpp
@@ -5,51 +5,45 @@
 #include <vector>    // for vector
 
 auto Lexer::getToken() -> Token {
+    // cout << "entered getToken()" << endl;
     if (*c == EOF) {
         return eof_;
     }
-    // cout << "entered getToken()" << endl;
-    auto s = dfa_.GetStart();
-    auto f = dfa_.GetTokenId(s);
-    // cout << "starting in state " << s << ", type " << f << endl;
+
     const auto start = c;
-    auto lastFinal = c;
-    auto current = c;
     const auto dstate = dfa_.GetDeadState();
-    while (/**current != EOF && */ s != dstate) {
+
+    // "iterators"
+    auto s = dfa_.GetStart();
+    auto current = c;
+
+    // "accumulators"
+    auto f = dfa_.GetTokenId(s);
+    auto lastFinal = c;
+
+    // cout << "starting in state " << s << ", type " << f << endl;
+    while (s != dstate) {
         // cout << "got '" << showCharEscaped(*current) << "'" << endl;
         // auto _c = dfa_.symbolToId.at(*current);
-        const auto _c = dfa_.symbolToId(*current);
+
         // if (_c == scount) {
         //    throw std::runtime_error("invalid symbol '" + printEscaped(*current) + "'");
         //}
-        s = dfa_.GetTransition(s, _c);
-        const auto ff = dfa_.GetTokenId(s);
-        if (ff) {
+        s = dfa_.GetTransition(s, dfa_.symbolToId(*current));
+
+        if (const auto ff = dfa_.GetTokenId(s); ff) {
             f = ff;
             lastFinal = current;
         }
         // cout << "new state " << s << ", type " << fptr[s] << ", last final: " << f << endl;
         // cout << "read " << (current - start + 1) << ", current token length: " << (lastFinal - start + 1) << endl;
         ++current;
-        // cin.get();
     }
 
-    // if (*current == EOF)
-    //  cout << "got EOF" << endl;
-    // if (s == dstate)
-    //  cout << "ran into dead end" << endl;
-
-    if (f != 0) {
-        c = lastFinal + 1;
-        if (f != whitespace_)
-            return {start, static_cast<size_t>(lastFinal - start + 1), f};
-        else
-            return getToken();
-        //} else if (*current == EOF) {
-        //    c = current;
-        //    return eofToken();
-    } else {
+    if (f == 0) {
         throw std::runtime_error("Lexical error");
     }
+
+    c = lastFinal + 1;
+    return f != whitespace_ ? Token{start, static_cast<size_t>(lastFinal - start + 1), f} : getToken();
 }
